46890203c3d330b3facce1b93f3384f384d64e0e
CedarChennn <a1249812431@gmail.com>
2019-09-08 00:40:14 +0800

    add function isValid

--- PythonApplication.py
+++ PythonApplication.py
@@ -1,5 +1,39 @@
 
 import random
+def isValid(self, s):
+    """
+    :type s: str
+    :rtype: bool
+    """
+
+    # The stack to keep track of opening brackets.
+    stack = []
+
+    # Hash map for keeping track of mappings. This keeps the code very clean.
+    # Also makes adding more types of parenthesis easier
+    mapping = {")": "(", "}": "{", "]": "["}
+
+    # For every bracket in the expression.
+    for char in s:
+
+        # If the character is an closing bracket
+        if char in mapping:
+
+            # Pop the topmost element from the stack, if it is non empty
+            # Otherwise assign a dummy value of '#' to the top_element variable
+            top_element = stack.pop() if stack else '#'
+
+            # The mapping for the opening bracket in our hash and the top
+            # element of the stack don't match, return False
+            if mapping[char] != top_element:
+                return False
+        else:
+            # We have an opening bracket, simply push it onto the stack.
+            stack.append(char)
+
+    # In the end, if the stack is empty, then we have a valid expression.
+    # The stack won't be empty for cases like ((()
+    return not stack
 def quiclsort(numbers):
     if len(numbers) <= 1:
            return numbers
@@ -14,8 +48,8 @@ def quiclsort(numbers):
             right.append(li)
     return quiclsort(left) + mid + quiclsort(right)
 def main():
-    numbers = [3,4,5,6,6,4,8,5,9,1,2,6]
-    for i in range(10):
-        print(quiclsort(numbers))
+    mapping = {")": "(", "}": "{", "]": "["}
+    for char in mapping:
+        print(char)
 if __name__ == '__main__':
     main()
